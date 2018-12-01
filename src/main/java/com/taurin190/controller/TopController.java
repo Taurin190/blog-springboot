6e6caf1516585558fa5d2fff569d7866ea048337
Koichi Taura <taura.koichi@gmail.com>
2018-12-01 23:34:18 +0900

    prepare to debug

--- src/main/java/com/taurin190/controller/TopController.java
+++ src/main/java/com/taurin190/controller/TopController.java
@@ -1,4 +1,18 @@
 package com.taurin190.controller;
 
-public class TopController {
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@EnableAutoConfiguration
+public class TopController extends ExceptionHandleController {
+    @GetMapping("/")
+    public ModelAndView index(ModelAndView mav) {
+        mav.setViewName("index.html");
+        return mav;
+    }
 }
