8770b9e51a6955f87408dec283712a72c78809bf
Koichi Taura <taura.koichi@gmail.com>
2018-12-28 09:57:14 +0900

    modify index and detail page to use entity information

--- src/main/java/com/taurin190/controller/TopController.java
+++ src/main/java/com/taurin190/controller/TopController.java
@@ -1,5 +1,8 @@
 package com.taurin190.controller;
 
+import com.taurin190.entity.AuthorEntity;
+import com.taurin190.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +11,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @EnableAutoConfiguration
 public class TopController {
+    @Autowired
+    AuthorService authorService;
+
     @GetMapping("/")
     public ModelAndView index(ModelAndView mav) {
+        AuthorEntity entity = authorService.getAuthorEntityById(new Integer(1));
         mav.setViewName("index.html");
+        mav.addObject("author", entity);
         return mav;
     }
 }
--- src/main/java/com/taurin190/service/AuthorService.java
+++ src/main/java/com/taurin190/service/AuthorService.java
@@ -1,4 +1,18 @@
 package com.taurin190.service;
 
+import com.taurin190.entity.AuthorEntity;
+import com.taurin190.exception.NotFoundException;
+import com.taurin190.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthorService {
+    @Autowired
+    AuthorRepository authorRepository;
+
+    public AuthorEntity getAuthorEntityById(Integer id) {
+        AuthorEntity entity = authorRepository.findById(id).orElseThrow(NotFoundException::new);
+        return entity;
+    }
 }
