bce13c4dcd3c54581ca2112e3d3f032a1420d9c7
Koichi Taura <taura.koichi@gmail.com>
2018-12-29 21:41:56 +0900

    add display setting to output from blog database

--- src/main/java/com/taurin190/controller/TopController.java
+++ src/main/java/com/taurin190/controller/TopController.java
@@ -1,12 +1,16 @@
 package com.taurin190.controller;
 
 import com.taurin190.entity.AuthorEntity;
+import com.taurin190.entity.BlogEntity;
 import com.taurin190.service.AuthorService;
+import com.taurin190.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @EnableAutoConfiguration
@@ -14,11 +18,16 @@ public class TopController {
     @Autowired
     AuthorService authorService;
 
+    @Autowired
+    private BlogService blogService;
+
     @GetMapping("/")
     public ModelAndView index(ModelAndView mav) {
         AuthorEntity entity = authorService.getAuthorEntityById(new Integer(1));
+        List<BlogEntity> entityList = blogService.getAllBlogEntity();
         mav.setViewName("index.html");
         mav.addObject("author", entity);
+        mav.addObject("blog_list", entityList);
         return mav;
     }
 }
