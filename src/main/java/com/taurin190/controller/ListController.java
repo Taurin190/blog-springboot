e671927e9a469b40f58d51f7686760f2afb72223
Koichi Taura <taura.koichi@gmail.com>
2018-12-29 19:10:51 +0900

    add get entity function and attach it to mav

--- src/main/java/com/taurin190/controller/ListController.java
+++ src/main/java/com/taurin190/controller/ListController.java
@@ -1,5 +1,8 @@
 package com.taurin190.controller;
 
+import com.taurin190.entity.BlogEntity;
+import com.taurin190.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
@@ -7,15 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 @EnableAutoConfiguration
 public class ListController {
+    @Autowired
+    private BlogService blogService;
+
     @GetMapping("/blog")
     public ModelAndView list(
             @Nullable
             @RequestParam("p") String page,
             ModelAndView mav) {
+        List<BlogEntity> entityList = blogService.getAllBlogEntity();
         mav.setViewName("blog_list.html");
+        mav.addObject("blog_list", entityList);
         return mav;
     }
 }
--- src/main/java/com/taurin190/service/BlogService.java
+++ src/main/java/com/taurin190/service/BlogService.java
@@ -3,8 +3,13 @@ package com.taurin190.service;
 import com.taurin190.entity.BlogEntity;
 import com.taurin190.exception.NotFoundException;
 import com.taurin190.repository.BlogRepository;
+import javassist.runtime.Desc;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BlogService {
@@ -14,5 +19,11 @@ public class BlogService {
     public BlogEntity getBlogByID(int id) {
         BlogEntity entity = blogRepository.findById(new Integer(id)).orElseThrow(NotFoundException::new);
         return entity;
+    }
+
+    public List<BlogEntity> getAllBlogEntity() {
+        List<BlogEntity> blogEntities = new ArrayList<>();
+        blogEntities = blogRepository.findAll(new Sort(Sort.Direction.DESC, "publishedDate"));
+        return blogEntities;
     }
 }
