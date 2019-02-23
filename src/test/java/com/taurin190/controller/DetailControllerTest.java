5af70bed4cc010017c4ed120760db028b1e6a177
Koichi Taura <taura.koichi@gmail.com>
2019-02-23 16:41:48 +0900

    add detail controller test actual test

--- src/test/java/com/taurin190/controller/DetailControllerTest.java
+++ src/test/java/com/taurin190/controller/DetailControllerTest.java
@@ -13,6 +13,8 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.ModelAndView;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -36,13 +38,30 @@ public class DetailControllerTest extends BaseTest{
         BlogEntity blogEntity = getTestBlogEntity();
         HeadEntity headEntity = getTestHeadEntity();
 
-        when(headService.getHeadEntity(any(String.class))).thenReturn(headEntity);
         when(blogService.getBlogByEnglishTitle(any(String.class))).thenReturn(blogEntity);
+        when(headService.getDetailHeadEntity("detail", blogEntity)).thenReturn(headEntity);
 
         ModelAndView mav = new ModelAndView();
         ModelAndView actual = detailController.detail("test", null, mav);
         ModelMap map = actual.getModelMap();
 
+        assertEquals("detail.html", actual.getViewName());
+        assertTrue(map.containsKey("blog"));
+        BlogEntity actualBlogEntity = (BlogEntity) map.get("blog");
+        assertTrue(map.containsKey("head_object"));
+        HeadEntity actualHeadEntity = (HeadEntity) map.get("head_object");
+
+        // assert HeadEntity
+        assertEquals("BLOG TEST - Title", actualHeadEntity.getTitle());
+        assertEquals("This is test page", actualHeadEntity.getDescription());
+        assertEquals("test", actualHeadEntity.getKeyword());
+        assertEquals("http://taurin190.com", actualHeadEntity.getShareUrl());
+
+        // assert List<BlogEntity>
+        assertEquals(new Integer(1), actualBlogEntity.getId());
+        assertEquals("AAAA BBB CC 1", actualBlogEntity.getBlogBody());
+        assertEquals("TEST_BLOG:1", actualBlogEntity.getEnglishTitle());
+        assertEquals("テストブログ1", actualBlogEntity.getTitle());
 
     }
 }
