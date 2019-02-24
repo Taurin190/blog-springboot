60f8f33127d2e3863a488b9b0a2bf940882c253b
Koichi Taura <taura.koichi@gmail.com>
2019-02-24 17:28:26 +0900

    delete unnecessary import

--- src/test/java/com/taurin190/service/HeadServiceTest.java
+++ src/test/java/com/taurin190/service/HeadServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.Assert.assertEquals;
 
