7828eaf914bc7b3315ffb4adb95de515177391ce
Koichi Taura <taura.koichi@gmail.com>
2019-03-02 14:46:56 +0900

    add test of list controller

--- src/test/java/com/taurin190/controller/ListControllerTest.java
+++ src/test/java/com/taurin190/controller/ListControllerTest.java
@@ -1,6 +1,9 @@
 package com.taurin190.controller;
 
 import com.taurin190.BaseTest;
+import com.taurin190.entity.AuthorEntity;
+import com.taurin190.entity.BlogEntity;
+import com.taurin190.entity.HeadEntity;
 import com.taurin190.service.AuthorService;
 import com.taurin190.service.BlogService;
 import com.taurin190.service.HeadService;
@@ -12,6 +15,13 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 public class ListControllerTest extends BaseTest {
     @Mock
@@ -33,8 +43,52 @@ public class ListControllerTest extends BaseTest {
 
     @Test
     public void list() {
+        AuthorEntity authorEntity = getTestAuthorEntity();
+        HeadEntity headEntity = getTestHeadEntity();
+        List<BlogEntity> blogEntityList = getTestBlogEntityList();
+
+        when(authorService.getAuthorEntityById(any(Integer.class))).thenReturn(authorEntity);
+        when(headService.getHeadEntity(any(String.class))).thenReturn(headEntity);
+        when(blogService.getAllBlogEntity()).thenReturn(blogEntityList);
+
         ModelAndView mav = new ModelAndView();
         ModelAndView actual = listController.list(null, mav);
         ModelMap map = actual.getModelMap();
+
+        assertEquals("blog_list.html", actual.getViewName());
+        assertTrue(map.containsKey("author"));
+        AuthorEntity actualAuthorEntity = (AuthorEntity) map.get("author");
+        assertTrue(map.containsKey("head_object"));
+        HeadEntity actualHeadEntity = (HeadEntity) map.get("head_object");
+        assertTrue(map.containsKey("blog_list"));
+        List<BlogEntity> actualBlogEntityList = (List<BlogEntity>) map.get("blog_list");
+
+        // assert AuthorEntity
+        assertEquals("Koichi Taura", actualAuthorEntity.getAuthorName());
+        assertEquals("Nice to meet you", actualAuthorEntity.getSelfIntroduction());
+        assertEquals("https://github.com/Taurin190", actualAuthorEntity.getGithubAccount());
+        assertEquals("https://twitter.com/Tauitter51", actualAuthorEntity.getTwitterAccount());
+        assertTrue(actualAuthorEntity.isValid());
+
+        // assert HeadEntity
+        assertEquals("BLOG TEST - Title", actualHeadEntity.getTitle());
+        assertEquals("This is test page", actualHeadEntity.getDescription());
+        assertEquals("test", actualHeadEntity.getKeyword());
+        assertEquals("http://taurin190.com", actualHeadEntity.getShareUrl());
+
+        // assert List<BlogEntity>
+        assertEquals(10, actualBlogEntityList.size());
+        assertEquals(new Integer(1), actualBlogEntityList.get(0).getId());
+        assertEquals("AAAA BBB CC 1", actualBlogEntityList.get(0).getBlogBody());
+        assertEquals("TEST_BLOG:1", actualBlogEntityList.get(0).getEnglishTitle());
+        assertEquals("テストブログ1", actualBlogEntityList.get(0).getTitle());
+        assertEquals(new Integer(5), actualBlogEntityList.get(4).getId());
+        assertEquals("AAAA BBB CC 5", actualBlogEntityList.get(4).getBlogBody());
+        assertEquals("TEST_BLOG:5", actualBlogEntityList.get(4).getEnglishTitle());
+        assertEquals("テストブログ5", actualBlogEntityList.get(4).getTitle());
+        assertEquals(new Integer(10), actualBlogEntityList.get(9).getId());
+        assertEquals("AAAA BBB CC 10", actualBlogEntityList.get(9).getBlogBody());
+        assertEquals("TEST_BLOG:10", actualBlogEntityList.get(9).getEnglishTitle());
+        assertEquals("テストブログ10", actualBlogEntityList.get(9).getTitle());
     }
 }
